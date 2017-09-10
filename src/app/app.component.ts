94cebb2adc5af5798433bef210b4fc0568ca4742
tomgdow <thomasgdowling@gmail.com>
2017-09-10 19:08:44 +0100

    initial setup with zurb-foundation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+import 'foundation-sites';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  ngAfterViewInit() {
+        $(document).foundation();
+  }
 }
